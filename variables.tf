9cf02a22951df79d4ae3ad430e520fa305e6af71
root <root@ip-172-31-17-167.ec2.internal>
2023-04-03 05:05:57 +0000

    keys added

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
-  default = "XXXXXXXXX"
+  default = "AKIAXW2NPSHLXL7GVDRR"
   }
 variable "secret_key" {
-  default = "XXXXXXX"
+  default = "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"
   }
 variable "region" {
-  default = "XXXXXXXXXX"
+  default = "us-east-1"
   }
